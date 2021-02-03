2b845707bb7fa92360e7f4b92ae36dda1d52e3c6
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-02 14:53:25 -1000

    using bracket notation to access arrays

--- dataStructures.js
+++ dataStructures.js
@@ -1,4 +1,9 @@
 // Most basic array containing multiple data types [string, number, boolean, string, null]
 let yourArray = ['five', 7, true, 'nothing', null];
 
+// Accessing an array using bracket notation
+let myArray = ["a", "b", "c", "d"];
+myArray[1] = "turtle";
+console.log(myArray);
+
 //
