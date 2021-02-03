740c963cf2ffa79a9a4c37205cd75695d8b0f77a
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-02 16:22:47 -1000

    Modifying a nested object

--- dataStructures.js
+++ dataStructures.js
@@ -109,4 +109,18 @@ foods.strawberries = 27;
 
 console.log(foods);
 
+// Modify an object nested within an object
+let userActivity = {
+    id: 23894201352,
+    date: 'January 1, 2017',
+    data: {
+      totalUsers: 51,
+      online: 42
+    }
+};
+
+userActivity.data.online = 45;
+
+console.log(userActivity);
+
 //
