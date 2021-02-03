eaa1a0f95d1862e08d33066f4d6f84e1b08f526f
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-02 15:41:38 -1000

    combine arrays with spread operator

--- dataStructures.js
+++ dataStructures.js
@@ -59,3 +59,12 @@ function copyMachine(arr, num) {
 console.log(copyMachine([true, false, true], 2));
 
 // Combine arrays with the spread operator
+function spreadOut() {
+    let fragment = ['to', 'code'];
+    let sentence = ['learning', ...fragment, 'is', 'fun'];
+    return sentence;
+}
+
+console.log(spreadOut());
+
+//
