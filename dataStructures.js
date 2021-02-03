4aa0a3aaae2329589acc1def14451a3e52d28d27
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-02 14:59:34 -1000

    unshift() and push() to add items to arrays

--- dataStructures.js
+++ dataStructures.js
@@ -6,4 +6,14 @@ let myArray = ["a", "b", "c", "d"];
 myArray[1] = "turtle";
 console.log(myArray);
 
-//
+// Add items to an array with push()*end of array* and unshift()*beginning of arrary*
+function mixedNumbers(arr) {
+    arr.unshift('I', 2, 'three');
+    arr.push(7, 'VIII', 9);
+    return arr;
+  }
+  
+  console.log(mixedNumbers(['IV', 5, 'six']));
+  // Notice the unshift and push MUST apply to arr not mixedNumbers
+
+  //
